d4e91c438cc3c608136400d8eb91f4a5df1ad3a3
mehmet313131131313 <100062693+mehmet313131131313@users.noreply.github.com>
2022-09-22 21:01:26 +0300

    Add files via upload

---
+++ app/emoji.js
@@ -0,0 +1,16 @@
+module.exports = {
+    onayemoji: ":tada: ",
+     redemoj: ":tada: ", 
+     tedoa:":tada: ",
+     sıfır:":tada: ",
+     bir:":tada:",
+     iki:":tada: ",
+     üc:":tada: ",
+     dört:":tada: ",
+     beş:":tada: ",
+     altı:":tada: ",
+     yedi:":tada: ",
+     sekiz:":tada: ",
+     dokuz:":tada: "
+  
+}
